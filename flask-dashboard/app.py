0e25c503d722f83621348bd2ff0f1f06d4b44a66
Courtney Shearer <csheare@clemson.edu>
2017-11-04 20:18:44 -0400

    apis for dayz1

--- flask-dashboard/app.py
+++ flask-dashboard/app.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template, json, request
-
+from flask import Flask, render_template, json, requests
+#key=0c02520f1b471cbda395f4f5a96fc8c4
 app = Flask(__name__)
 
 @app.route('/')
 def main():
+    response = requests.get("https://api.themoviedb.org/3/discover/movie?api_key=0c02520f1b471cbda395f4f5a96fc8c4&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=War")
+    # Print the status code of the response.
+    print(response.json()['genres'])
     return render_template('index.html')
 
 @app.route('/page2/', methods=['POST'])
