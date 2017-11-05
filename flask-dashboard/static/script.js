49673879dfd52f8dff7c91e57e5d51412c16dc83
Patrick Gorospe <prgorospe@gmail.com>
2017-11-04 22:29:15 -0400

    additions to page 1 and page 2 of Movie selection tool. Added assets, script js, and styles css

---
+++ flask-dashboard/static/script.js
@@ -0,0 +1,36 @@
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() {
+  $('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+}, 3000);
+
+
+$('tr').hide().filter(function () {
+    return $(this).find('td[colspan]').length;
+}).addClass('header2').css('display', 'table-row').click(function () {
+    $(this).nextUntil('tr.header2').css('display', function (i, v) {
+        return this.style.display === 'table-row' ? 'none' : 'table-row';
+    });
+});
+
+function addToMovieChoices(choice) {
+  if(document.getElementById('choice1') === 'choose a movie') {
+    document.getElementById('choice1') = choice;
+  } elseif (document.getElementById('choice2') === 'choose a movie' {
+    document.getElementById('choice1') = choice;
+  } else {
+    console.log("You've already made your two choices. Move on or deselect one.")
+  }
+}
+
+
+document.getElementById("slide1").addEventListener("click", addToMovieChoices(document.getElementById('slide1')));
+document.getElementById("slide2").addEventListener("click", addToMovieChoices(document.getElementById('slide2'));
+document.getElementById("slide3").addEventListener("click", addToMovieChoices(document.getElementById('slide3'));
+document.getElementById("slide4").addEventListener("click", addToMovieChoices(document.getElementById('slide4'));
+document.getElementById("slide5").addEventListener("click", addToMovieChoices(document.getElementById('slide5'));
